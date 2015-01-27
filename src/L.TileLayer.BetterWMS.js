caa2a9d089ea8f4b25ffae472c0f125005526b87
Mbrownshoes <mathewbrownemail@gmail.com>
2015-01-27 18:33:23 -0500

    time series plot time

--- src/L.TileLayer.BetterWMS.js
+++ src/L.TileLayer.BetterWMS.js
@@ -1,3 +1,4 @@
+var chartData = [];
 L.TileLayer.BetterWMS = L.TileLayer.WMS.extend({
 
   onAdd: function (map) {
@@ -18,12 +19,9 @@ getFeatureInfo: function (evt) {
     // Make an AJAX request to the server 
     var url = this.getFeatureInfoUrl(evt.latlng),
     showResults = L.Util.bind(this.showGetFeatureInfo, this);
-    console.log(url)
     $.ajax({
       url: url,
       success: function (data, status, xhr) {
-
-        // console.log(data)
         var err = typeof data === 'string' ? null : data;
         showResults(err, evt.latlng, data);
     },
@@ -37,12 +35,12 @@ getFeatureInfoUrl: function (latlng) {
     // Construct a GetFeatureInfo request URL given a point
     var point = this._map.latLngToContainerPoint(latlng, this._map.getZoom()),
     size = this._map.getSize(),
-    
     params = {
       request: 'GetFeatureInfo',
       service: 'WMS',
       srs: 'EPSG:4326',
-      time: this.wmsParams.time,
+      // time: this.wmsParams.time,
+      time: ["1985-1-15","1985-2-15","1985-3-15","1985-4-15","1985-5-15","1985-6-15","1985-7-15","1985-8-15","1985-9-15","1985-10-15","1985-11-15","1985-12-15"],
       styles: this.wmsParams.styles,
       transparent: this.wmsParams.transparent,
       version: this.wmsParams.version,      
@@ -62,10 +60,45 @@ getFeatureInfoUrl: function (latlng) {
 },
 
 showGetFeatureInfo: function (err, latlng, content) {
-    // console.log(content.documentElement.innerHTML)
+   console.log(content.getElementsByTagName('time')[0].innerHTML.substr(0,10))
+    // loop though time / values and create object
+    for (i = 0; i <12; i++) { 
+      chartData.push({
+        x:content.getElementsByTagName('time')[i].innerHTML.substr(0,10),
+        y: Number(content.getElementsByTagName('value')[i].innerHTML)})
+  }
 
+
+// make graph here (move later)
+
+
+var parseDate = d3.time.format("%Y-%m-%d").parse;
+
+// var graph = new Rickshaw.Graph( {
+//         element: document.querySelector("#chart"),
+//         renderer: 'line',
+//         width: 800,
+//         height: 250,
+//         series: [ {
+//                 color: 'steelblue',
+//                 data: chartData
+//         } ]
+// } );
+
+// var y_ticks = new Rickshaw.Graph.Axis.Y( {
+//     graph: graph,
+//     orientation: 'left',
+//     tickFormat: Rickshaw.Fixtures.Number.formatKMBT,
+//     element: document.getElementById('y_axis'),
+// } );
+
+
+// graph.render();
+//
+
+// console.log(content.getElementsByTagName('time'))
     // Otherwise show the content in a popup, or something.
-    
+  // chartData =content.getElementsByTagName('value');
 
     L.popup({ maxWidth: 800})
       .setLatLng(latlng)
